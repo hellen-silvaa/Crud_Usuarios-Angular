1f88d824ba4f130ad07501d979a72bc4cec25e8f
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-07 16:52:47 -0300

    icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonComponent } from './components/button/button.component';
+import { LoginComponent } from './pages/login/login.component';
+import {FormsModule} from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MatIconModule } from "@angular/material/icon";
 // import {LoginComponent} from './login'
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    LoginComponent,
+    HomeComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatIconModule,
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  providers: [ ],
+   // provideAnimationsAsync()
   bootstrap: [AppComponent]
 })
 export class AppModule { }
